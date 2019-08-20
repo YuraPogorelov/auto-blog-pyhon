e63d7d1e6d8b497da1846fcdf936ee419cabf481
YuraPogorelov <pogorelov.yurik@gmail.com>
2019-08-20 22:18:33 +0300

    Modul добавлен в admin панель

--- service/models.py
+++ service/models.py
@@ -7,8 +7,8 @@ class Category(models.Model):
 
     ''' Кактегория услуг '''
 
-    name = models.CharField('Название категории', max_legth=120)
-    slug = models.SlugField('URL', max_legth=120)
+    name = models.CharField('Название категории', max_length=120)
+    slug = models.SlugField('URL', max_length=120)
     text = models.TextField('Текст категории')
     banner = models.ImageField('Баннер', upload_to='images/', blank=True, null=True)
     title = models.CharField('Title', max_length=120)
