a12b62d09db1380962b7b15c019971f380223797
Yura Pogorelov <pyurik44@gmail.com>
2019-08-20 16:54:29 +0300

    создание модели Category

---
+++ service/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Category(models.Model):
+
+    ''' Кактегория услуг '''
+
+    name = models.CharField('Название категории', max_legth=120)
+    slug = models.SlugField('URL', max_legth=120)
+    text = models.TextField('Текст категории')
+    banner = models.ImageField('Баннер', upload_to='images/', blank=True, null=True)
+    title = models.CharField('Title', max_length=120)
+    description = models.CharField('Description', max_length=120)
+    keywords = models.CharField('Keywords', max_length=250)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
