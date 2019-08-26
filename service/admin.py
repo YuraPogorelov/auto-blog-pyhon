0c235e8d029d649405d78c0f91abbcff8298d906
Yura Pogorelov <pyurik44@gmail.com>
2019-08-26 15:10:20 +0300

    Настройка админки сайта

--- service/admin.py
+++ service/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from .models import Category, Model
 
+
 # Register your models here.
 
-admin.site.register(Category)
-admin.site.register(Model)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
+    save_on_top = True
+
+
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'category', 'sort', 'active')
+    list_display_links = ('name', 'slug', 'category', 'sort', 'active')
+    prepopulated_fields = {'slug': ('name',)}
+    list_filter = ['category', 'active']
+    save_on_top = True
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Model, ModelAdmin)
